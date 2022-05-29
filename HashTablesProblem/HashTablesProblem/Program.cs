885c5f7aa6f49e92c2422723d2861516a8a47206
prashantmagare <prashantmagare3010@gmail.com>
2022-05-29 23:53:27 +0530

    "Added - HashTableProgram"

---
+++ HashTablesProblem/HashTablesProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Hash Table Program");
+        }
+    }
+}
